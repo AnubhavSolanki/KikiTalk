38bae0c14ee4859e9ad2ec5d01d650c43e0b81a4
AnubhavSolanki <anubhavsolanki0@gmail.com>
2022-05-04 23:52:37 +0530

    fixed single page for post

--- frontend/src/components/completePost/completePost.js
+++ frontend/src/components/completePost/completePost.js
@@ -1,27 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import useWindowDimensions from "../../utils/useWindowDimensions";
 import CommentContainer from "../comment/commentContainer";
 import { PostFooter } from "../post/postFooter";
 import styles from "./completePost.module.css";
 
 const CompletePost = ({ options }) => {
+  const { width, height } = useWindowDimensions();
+  const [showState, setShowState] = useState({
+    leftPanel: true,
+    rightPanel: true,
+  });
+
   useEffect(() => {
-    document
-      .querySelector("body")
-      .style.setProperty("--explorePostSizeFactor", "130");
-    return () => {
-      document
-        .querySelector("body")
-        .style.setProperty("--explorePostSizeFactor", "120");
-    };
-  }, []);
+    if (width < 700 || height < 700) {
+      setShowState({
+        leftPanel: false,
+        rightPanel: true,
+      });
+    } else {
+      setShowState({
+        leftPanel: true,
+        rightPanel: true,
+      });
+    }
+  }, [width, height]);
   return (
     <div className={styles.wrapper}>
-      <PostFooter options={options} />
-      <CommentContainer
-        postDataUserId={options.postData.userId}
-        postId={options.postData._id}
-        history={options.history}
-      />
+      {showState.leftPanel && <PostFooter options={options} />}
+      {showState.rightPanel && (
+        <CommentContainer
+          postDataUserId={options.postData.userId}
+          postId={options.postData._id}
+          history={options.history}
+        />
+      )}
     </div>
   );
 };
